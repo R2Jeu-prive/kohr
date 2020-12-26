40182ed19bca68d6c64893bd3c4ef47fb36d8cba
Pierre <R2Jeu.prive@gmail.com>
2020-12-26 16:16:33 +0100

    fixed bug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on('connection', function(socket){
 
     socket.on('disconnect', function() {
         console.log(users.find(user => user.socket_id === socket.id).pseudo + " lost !")
-        delete users.find(user => user.socket_id === socket.id)
+        delete users[users.findIndex(user => user.socket_id === socket.id)]
         console.log(users);
     });
 });
