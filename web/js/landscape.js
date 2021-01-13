f3c3f0e54138e4f3c961314c1bf4578483fcd3ad
Pierre <R2Jeu.prive@gmail.com>
2021-01-13 17:37:21 +0100

    adding landscape-frame

--- web/js/landscape.js
+++ web/js/landscape.js
@@ -1,8 +1,8 @@
 $( window ).on( "orientationchange", function( event ) {
-    setLandscape()
+    setLandscape(true)
 });
-function setLandscape(){
-    if(window.innerHeight > window.innerWidth){
+function setLandscape(inverse){
+    if((window.innerHeight > window.innerWidth && !inverse) || (window.innerHeight < window.innerWidth && inverse)){
         $("#landscape-frame").attr("hidden",false);
     }else{
         $("#landscape-frame").attr("hidden",true);
