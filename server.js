63f229a442db0664606a0c6acf71f0fa34ac4e02
Pierre <R2Jeu.prive@gmail.com>
2021-02-07 18:16:57 +0100

    testing

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ io.on('connection', function(socket){
                     game.pieceBuild("Pawn",1,1,0,456456,io)
                     game.pieceBuild("Pawn",1,7,0,456456,io)
                     game.pieceBuild("Pawn",7,1,0,456456,io)
-                    game.pieceBuild("Pawn",2,3,0,456456,io)
+                    game.pieceBuild("Pawn",3,2,0,456456,io)
                 }else{
                     socket.emit("fatalError",{text : "Error #004 | Vous ne pouvez pas lancer cette partie !"});
                 }
