4fc9c301e5ee1a88519536f8dbc8f1332d1d3dd9
Pierre <R2Jeu.prive@gmail.com>
2021-03-13 12:52:05 +0100

    debugging

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -16,7 +16,7 @@ class Game {
         this.buildings = []
         this.players = []
         this.disconnectedPlayers = []
-        this.stats = [[1000,1000,1000,1000,0],[0,0,0,0,0]]
+        this.stats = [[0,1000,1000,1000,1000],[0,0,0,0,0]]
         this.lastTimeStamp = 0;
         this.skipId = undefined
     }
