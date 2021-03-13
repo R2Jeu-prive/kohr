8dcb8ea467ffd501502c9b03a98d44b7e6303ade
Pierre <R2Jeu.prive@gmail.com>
2021-03-13 11:35:56 +0100

    fixed bug

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -277,10 +277,7 @@ class Game {
         if(atMiddle){
             var neighbours = 0
             this.buildings.forEach(function(building){
-                console.log(building)
-                console.log(this)
-                console.log(Math.abs(building.x - this.x) + Math.abs(building.y - this.y) == 1 && building.atMiddle == this.atMiddle)
-                if(Math.abs(building.x - this.x) + Math.abs(building.y - this.y) == 1 && building.atMiddle == this.atMiddle){
+                if(Math.abs(building.x - x) + Math.abs(building.y - y) == 1 && building.atMiddle == atMiddle){
                     neighbours = neighbours + 1
                 }
             },this)
