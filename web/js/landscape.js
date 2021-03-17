2b61bcdfabaca7397aadf70a5a19c4da2b9c0927
Pierre <R2Jeu.prive@gmail.com>
2021-03-17 20:03:12 +0100

    testing new landscape detection

--- web/js/landscape.js
+++ web/js/landscape.js
@@ -2,9 +2,15 @@ $( window ).on( "orientationchange", function( event ) {
     setLandscape(true)
 });
 function setLandscape(inverse){
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        // you're in LANDSCAPE mode
+        $("#landscape-frame").attr("hidden",false);
+    }else{
+        $("#landscape-frame").attr("hidden",true);
+    }/*
     if((screen.availHeight > screen.availWidth && !inverse) || (screen.availHeight < screen.availWidth && inverse)){
         $("#landscape-frame").attr("hidden",false);
     }else{
         $("#landscape-frame").attr("hidden",true);
-    }
+    }*/
 }
